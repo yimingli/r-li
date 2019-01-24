f32109bb3cdb1a84a1245b1cf31f0060e7c47669
Yiming Paul Li <>
2019-01-23 23:30:07 -0800

    More documentation

--- R/check_constant.R
+++ R/check_constant.R
@@ -6,13 +6,17 @@
 #'
 #' @examples
 #' is_constant(1:10)
+#' is_not_constant(1:10)
 #'
 #'
 #' @export
+#' @rdname check_constant
 is_constant <- function(var) {
     all(var == var[1], na.rm = TRUE)
 }
 
+#' @export
+#' @rdname check_constant
 is_not_constant <- function(var) {
     !all(var == var[1], na.rm = TRUE)
 }
